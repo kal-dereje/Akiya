703c7a1b34fc48c364a7bab678faed83538a98c1
adoni9 <57844700+adoni9@users.noreply.github.com>
2022-10-01 20:54:44 +0300

    additional controller for update and sell

---
+++ Controllers/EditController.cs
@@ -0,0 +1,39 @@
+﻿using Akiya.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Akiya.Controllers
+{
+    public class EditController : Controller
+    {
+
+        private readonly projectnewContext _context;
+
+
+        public EditController(projectnewContext context)
+        {
+            this._context = context;
+        }
+        //EDIT PAGE
+
+        public IActionResult Index(int id)
+        {
+            if (HttpContext.Session.GetInt32("id") != null)
+                ViewBag.Id = HttpContext.Session.GetInt32("id");
+
+            var property = _context.Properties.Find(id);
+            return View(property);
+        }
+
+        [HttpPost]
+        public IActionResult Index(Property property)
+        {
+
+            property.FkUser = HttpContext.Session.GetInt32("id");
+            _context.Properties.Update(property);
+            _context.SaveChanges();
+            return RedirectToAction("post", "home");
+        }
+
+    }
+}
---
+++ Controllers/SellController.cs
@@ -0,0 +1,42 @@
+﻿using Akiya.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Akiya.Controllers
+{
+    public class SellController : Controller
+    {
+
+        private readonly projectnewContext _context;
+
+
+        public SellController(projectnewContext context)
+        {
+            this._context = context;
+        }
+        //SELL PAGE
+
+        public IActionResult Index()
+        {
+            if (HttpContext.Session.GetInt32("id") != null)
+                ViewBag.Id = HttpContext.Session.GetInt32("id");
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Property pro)
+        {
+            pro.FkUser = HttpContext.Session.GetInt32("id");
+            if (pro.FkUser != null || pro.FkUser > 0)
+            {
+                _context.Properties.Add(pro);
+                _context.SaveChanges();
+            }
+
+
+          
+            return RedirectToAction("index");
+        }
+
+    }
+}
