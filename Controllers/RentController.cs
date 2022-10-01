3658d6dd6fc9cedf5a6fe6e51f4eb4684adaeb7d
Abu-G <76791901+Abu-G@users.noreply.github.com>
2022-10-01 16:54:11 +0300

    i add rent and buy controllers

---
+++ Controllers/RentController.cs
@@ -0,0 +1,30 @@
+﻿using Akiya.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace Akiya.Controllers
+{
+    public class RentController : Controller
+    {
+
+        private readonly projectnewContext _context;
+
+
+        public RentController(projectnewContext context)
+        {
+            this._context = context;
+        }
+
+        //RENT PAGE
+        public IActionResult Index()
+        {
+            if (HttpContext.Session.GetInt32("id") != null)
+                ViewBag.Id = HttpContext.Session.GetInt32("id");
+
+            var property = _context.Properties.Where(temp => temp.Purpose == "rent").ToList();
+            return View(property);
+        }
+
+    }
+}
